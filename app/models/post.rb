87ec62466ecdbeb37b4fe882cb8c6826db7f3bb5
David Wenzel <dwenzel95@gmail.com>
2023-02-12 19:02:53 -0600

    commit tree fa4539aeb93914c001191e64f0d67fa2b137f448

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+#   validate :clickbait_title
+
+  
+#   clickbait_phrases = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+#   def clickbait_title
+#     if title.includes?{ |title| phrase.match title}
+#         errors.add(:title, "make it clickbait")
+#     end
+#   end
+
 end
